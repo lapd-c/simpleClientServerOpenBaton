e013b4025a819227c6428077aca0b0e990e50792
tulja <btuljavamshikiran@gmail.com>
2017-10-29 03:35:33 +0530

    Update startClient.sh

--- startClient.sh
+++ startClient.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-#wait for 10 seconds for server to become active   
+g++ client.cpp -o client
+
+#wait for 10 seconds for server to become active 
 sleep 10s 
 ./client $server_private 
