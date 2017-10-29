2707ca08c026b68ad6eb8b038196adea90c94249
tulja <btuljavamshikiran@gmail.com>
2017-10-29 13:35:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get install -y iperf screen g++
+echo "Compiling the client and server programs"
+g++ client.cpp -o client
+g++ server.cpp -o server
+echo "Done!"
