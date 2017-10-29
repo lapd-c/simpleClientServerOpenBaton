d595a14491f47dad023989922886184afa8c2ed0
tulja <btuljavamshikiran@gmail.com>
2017-10-29 18:27:22 +0530

    Update compileFiles.sh

--- compileFiles.sh
+++ compileFiles.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Change dir to Home Folder " 
+cd ~
+echo "Copy the Client and Server code to Home Folder"
+cp /opt/openbaton/scripts/client.cpp ~
+cp /opt/openbaton/scripts/server.cpp ~
 echo "Compiling the client and server programs"
 sudo g++ client.cpp -o client
 sudo g++ server.cpp -o server
