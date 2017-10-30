b36a0f3473f33900e2ea2c034fc2c4e8348d6cf3
tulja <btuljavamshikiran@gmail.com>
2017-10-30 08:18:31 +0530

    Update startClient.sh

--- startClient.sh
+++ startClient.sh
@@ -4,4 +4,4 @@ cd /opt/openbaton/scripts/
 echo "Client has started and is trying to connect to Server at port number 8089"
 #wait for 10 seconds for server to become active 
 sleep 10s 
-./client $server_private 
+screen -d -m -S client ./client $server_private 
