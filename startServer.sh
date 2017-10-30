b3a400839d837a9ad45296375d23cd2cbe080870
tulja <btuljavamshikiran@gmail.com>
2017-10-30 08:16:53 +0530

    Update startServer.sh

--- startServer.sh
+++ startServer.sh
@@ -3,4 +3,4 @@ echo "Change the Directory to /opt/openbaton/scripts"
 cd /opt/openbaton/scripts/
 echo "Server has started and is listening at port number 8089"
 #run the server
-./server
+screen -d -m -S server ./server
