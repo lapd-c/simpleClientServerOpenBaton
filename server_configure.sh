c6eaa4c974b56f81e234144ce01d28beab781896
tulja <btuljavamshikiran@gmail.com>
2017-11-01 10:39:03 +0530

    Update server_configure.sh

--- server_configure.sh
+++ server_configure.sh
@@ -4,4 +4,5 @@ cd /opt/openbaton/scripts/
 echo "Client has started and is trying to connect to Server at port number 8089"
 #wait for 10 seconds for server to become active 
 echo $server_private > server_ip.txt
-./client $server_private > client_output.txt
+echo $server_private2 > server2_ip.txt
+./client $server_private2 > client_output.txt
